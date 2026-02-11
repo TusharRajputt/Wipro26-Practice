9477ecdd226bfb4e2e82d02835d8f25a04c11324
Tushar <tusharrajput4490@gmail.com>
2026-02-11 16:26:10 +0530

    update

--- Day1/Lab2.cs
+++ Day1/Lab2.cs
@@ -1,34 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     internal class Lab2
     {
-        public void Show()
+        /**summary 
+         * Write a program to count no. of palindrome wordds in the given string.
+         * Welcome All liril is soap Malayalam madam is heading company
+        **/
+        public void CheckPaLindrome(string s)
         {
-            int[] arr = new int[10] { 22, 52, 11, 10, 12, 22, 32, 12, 34, 43 };
-           
-            int[] a = new int[] { 1, 2, 3, 4, 5, 6 };
+            string[] word = s.Split(' '); // split the sentence into words
+            int count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                string w = word[i].ToLower();  // converts the word to lower case
+                string rev = "";
 
-            for (int i = 0; i < arr.Length; i++) {
-                Console.WriteLine(arr[i]  );
+                for (int j = w.Length - 1; j >= 0; j--)
+                {
+                    rev = rev + w[j];
+                }
 
-              
+
+                if (w.Equals(rev))
+                {
+                    count++;
+                }
             }
-            Console.WriteLine("\n");
-            foreach (int i in a) {
-                Console.WriteLine(i);
-            }
+
+            Console.WriteLine("Number of palindrome words in the given string is " + count);
         }
 
+
         static void Main()
-           {
-            Lab2 a = new Lab2();
-            a.Show();
-            }
+        {
+            Console.WriteLine("Enter the string");
+            String s = Console.ReadLine();
+            Lab2 l = new Lab2();
+            l.CheckPaLindrome(s);
+        }
     }
 }
